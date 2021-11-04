f39312b2c6d9ac5a14364794d694bd121f1172db
jdwcg <bigcloudjo@gmail.com>
2021-11-04 23:41:15 +0900

    29

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-document.getElementById('popupBg').style.display='none';
 document.getElementById('popup').style.display='none';
+document.getElementById('popupBg').style.display='none';
 document.getElementById('openPop').addEventListener('click', function(){
-   document.getElementById('popup').style.display='block';
-   document.getElementById('popupBg').style.display='block';
+   if(document.getElementById('popup').style.display==='none') {
+      document.getElementById('popup').style.display='block';
+      document.getElementById('popupBg').style.display='block';
+   }
 })
 document.getElementById('closePop').addEventListener('click', function(){
    document.getElementById('popup').style.display='none';
