a2009fe7561a9f86576777b8f865493345b3ddc3
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-11-05 15:14:24 +0900

    47

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-document.getElementById('popup').style.display='none';
-document.getElementById('popupBg').style.display='none';
+// document.getElementById('popup').style.display='none';
+// document.getElementById('popupBg').style.display='none';
 document.getElementById('openPop').addEventListener('click', function(){
    if(document.getElementById('popup').style.display==='none') {
       document.getElementById('popup').style.display='block';
