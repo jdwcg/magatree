b838548095e41fab293f51c7d379c8a11576491a
jdwcg <73451502+jdwcg@users.noreply.github.com>
2021-11-05 13:10:40 +0900

    34

--- assets/script.js
+++ assets/script.js
@@ -29,9 +29,19 @@ $(function(){
   	}
   })
 
-
-
-
-
-
 })
+
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
